f5cdbf09d5f5686e47914fe8fb98c7ee4eb792f9
TorqueReborn <torquereborn@gmail.com>
2025-10-03 09:31:25 +0530

    Center items in the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen">
       You can trust Abhinand Ammanamkandy on this website.<br/>
       He will build this portfolio
     </div>
