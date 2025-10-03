5352fff2cd736dbeddd2545bfc8142ae5ad1fa8e
TorqueReborn <torquereborn@gmail.com>
2025-10-03 09:29:08 +0530

    Initial Template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      You can trust Abhinand Ammanamkandy on this website.<br/>
+      He will build this portfolio
+    </div>
+  )
+}
+
+export default App
